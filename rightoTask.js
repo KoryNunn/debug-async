0b76c71fac7d8bea2c974dd1e5c43d584385ef49
Kory Nunn <knunn187@gmail.com>
2017-06-05 19:46:26 +1000

    initial;

---
+++ rightoTask.js
@@ -0,0 +1,21 @@
+var righto = require('righto');
+var fs = require('fs');
+
+var pathsFile = righto(fs.readFile, __dirname + '/test.txt', 'utf8');
+
+var fileNames = pathsFile.get(function(filePaths){
+    return filePaths.spilt('\n');
+});
+
+var files = fileNames.get(function(names){
+        return righto.all(names.map(function(name){
+            var filePath = __dirname + '/' + name + '.txt';
+            return righto(fs.readFile, filePath, 'utf8');
+        }));
+    });
+
+files(function(error, files){
+    files.forEach(function(file){
+        console.log(file);
+    });
+});
