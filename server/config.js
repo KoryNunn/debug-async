8f5a53cbd8aa4e2633066f05bec7d46cf05d3138
Kory Nunn <knunn187@gmail.com>
2019-11-26 22:56:55 +1000

    Prodified config

--- server/config.js
+++ server/config.js
@@ -1,10 +1,10 @@
 module.exports = {
     port: process.env['PORT'] || 8080,
     databaseToken: process.env['DATABASE_TOKEN'] || 'test',
-    bitabaseEndpoint: 'http://localhost:8000/v1/databases/asyncdebuggingresults',
-    // bitabaseEndpoint: 'https://api.bitabase.net/v1/databases/asyncdebuggingresults',
+    // bitabaseEndpoint: 'http://localhost:8000/v1/databases/asyncdebuggingresults',
+    bitabaseEndpoint: 'https://api.bitabase.net/v1/databases/asyncdebuggingresults',
     bitabaseSessionId: process.env['BITABASE_SESSION_ID'],
     bitabaseSessionSecret: process.env['BITABASE_SESSION_SECRET'],
-    collectionEndpoint: collection => `http://localhost:8000/v1/databases/asyncdebuggingresults/records/${collection}`
-    // collectionEndpoint: collection => `https://asyncdebuggingresults.bitabase.net/${collection}`
+    // collectionEndpoint: collection => `http://localhost:8000/v1/databases/asyncdebuggingresults/records/${collection}`
+    collectionEndpoint: collection => `https://asyncdebuggingresults.bitabase.net/${collection}`
 }
