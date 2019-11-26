2e2f53db635748029e0ae5a725b9a2af4866d530
Kory Nunn <knunn187@gmail.com>
2019-11-26 23:13:35 +1000

    change port env var??

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env['PORT'] || 8080,
+    port: process.env['HOST_PORT'] || 8080,
     databaseToken: process.env['DATABASE_TOKEN'] || 'test',
     // bitabaseEndpoint: 'http://localhost:8000/v1/databases/asyncdebuggingresults',
     bitabaseEndpoint: 'https://api.bitabase.net/v1/databases/asyncdebuggingresults',
